918a0c56bb0d69b501be4188f6237e8fc2fbc295
Hubert Quarshie <huberts.quarshie@gmail.com>
2025-06-06 12:24:52 +0000

    meh

--- app/(root)/leads/page.tsx
+++ app/(root)/leads/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from 'next'
 import AddButton from '@/components/shared/add-button'
-import { DataTable } from '@/components/shared/data-table'
-import { leadsColumns } from './leads-columns'
+// import { DataTable } from '@/components/shared/data-table'
+// import { leadsColumns } from './leads-columns'
 import { fetchLeads } from '@/lib/data/leads.data'
-// import TestTable from './test-table'
+import TestTable from './test-table'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 export const metadata: Metadata = {
 	title: 'Leads management',
@@ -26,10 +27,40 @@ const LeadsPage = async () => {
 
 			{/* <TestTable /> */}
 
-			<DataTable
+			<Table>
+				<TableCaption>A list of leads</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>First name</TableHead>
+						<TableHead>Last name</TableHead>
+						<TableHead>Email</TableHead>
+						<TableHead>Phone</TableHead>
+						<TableHead>Status</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{leads.length > 0 ? (
+						leads.map(lead => (
+							<TableRow key={lead.id}>
+								<TableCell>{lead.firstName}</TableCell>
+								<TableCell>{lead.lastName}</TableCell>
+								<TableCell>{lead.email}</TableCell>
+								<TableCell>{lead.phone}</TableCell>
+								<TableCell>{lead.status}</TableCell>
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={6}>No data</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+
+			{/* <DataTable
 				data={leads}
 				columns={leadsColumns}
-			/>
+			/> */}
 		</main>
 	)
 }
